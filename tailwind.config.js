0e22520b317b364ea6132f362643e55040233247
Michael Ebube <michaelebube18@gmail.com>
2024-09-02 04:34:06 +0100

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+   safelist: [
+    'text-gray-950', 
+  ],
   theme: {
     extend: {
       
