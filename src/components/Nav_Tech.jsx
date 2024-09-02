b6664615dbf2d4b6c3331929dba1157514485f2e
Michael Ebube <michaelebube18@gmail.com>
2024-09-02 05:04:43 +0100

    new commit

--- src/components/Nav_Tech.jsx
+++ src/components/Nav_Tech.jsx
@@ -16,9 +16,9 @@ const Nav_Tech = ({ selectedResource, handleClick }) => {
             >
               <div
                 className={` xs:w-[10px] xs:h-[10px] lg:w-[200px] lg:h-[200px] p-5 sm:p-6   md:p-7 xl:p-8 rounded-full text-xl xl:text-2xl ${
-                  isSelected('LAUNCH VEHICLE') ? 'bg-white text-gray-950 bg-opacity-100 z-10 ' : ''
+                  isSelected('LAUNCH VEHICLE') ? 'bg-white text-gray-950 bg-opacity-100 z-10 ' : 'text-white'
                 } transition-all duration-300 ease-in-out   flex items-center justify-center
-                text-white border  border-white/30
+                 border  border-white/30
                 hover:border-white hover:scale-105`}
               >1</div>
             </NavLink>
@@ -33,9 +33,9 @@ const Nav_Tech = ({ selectedResource, handleClick }) => {
                 className={` xs:w-[10px] xs:h-[10px] lg:w-10 lg:h-10 p-5 sm:p-6 md:p-7 xl:p-8   rounded-full text-xl xl:text-2xl ${
                   isSelected('SPACEPORT')
                     ? 'bg-white text-gray-950 bg-opacity-100 z-10'
-                    : ' '
+                    : 'text-white '
                 } transition-all duration-300 ease-in-out   flex items-center justify-center
-                text-white border  border-white/30
+                 border  border-white/30
                 hover:border-white hover:scale-105`}
               >2</div>
             </NavLink>
@@ -48,10 +48,10 @@ const Nav_Tech = ({ selectedResource, handleClick }) => {
             >
               <div
                 className={`xs:w-[10px] xs:h-[10px] lg:w-10 lg:h-10 p-5 sm:p-6 md:p-7 xl:p-8   rounded-full text-xl xl:text-2xl ${
-                  isSelected('SPACE CAPSULE') ? 'bg-white text-gray-950 bg-opacity-100 z-10' : ''
+                  isSelected('SPACE CAPSULE') ? 'bg-white text-gray-950 bg-opacity-100 z-10' : 'text-white'
                 } transition-all duration-300 ease-in-out  flex items-center justify-center border  border-white/30
                 hover:border-white hover:scale-105
-                 text-white `}
+                  `}
               >3</div>
             </NavLink>
           </li>
